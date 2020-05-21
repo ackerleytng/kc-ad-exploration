ebe2e0024a2e6c327a11ac50f7d718d57bacee6a
Ackerley Tng <ackerleytng@gmail.com>
2020-05-21 08:42:32 +0800

    Add additional findings in README.md

--- provision-01-add-users-groups.ps1
+++ provision-01-add-users-groups.ps1
@@ -1,6 +1,6 @@
-# ------------------------------------------------------
+# --------------------------------------------------------
 #   Add Ruby group and User Zero, User One
-# ------------------------------------------------------
+# --------------------------------------------------------
 
 New-ADGroup -Name "Ruby" -SamAccountName Ruby -GroupCategory Security -GroupScope Global -DisplayName "Ruby" -Path "CN=Users,DC=vdom,DC=local"
 
@@ -12,9 +12,9 @@ New-ADUser -Name "User One" -GivenName "User" -Surname "One" -SamAccountName "us
 
 Add-ADGroupMember -Identity $(Get-ADGroup -Identity "CN=Ruby,CN=Users,DC=vdom,DC=local") -Members $(Get-ADUser -Identity "CN=User One,CN=Users,DC=vdom,DC=local")
 
-# ------------------------------------------------------
+# --------------------------------------------------------
 #   Add Sapphire group and User Two, User Three
-# ------------------------------------------------------
+# --------------------------------------------------------
 
 New-ADGroup -Name "Sapphire" -SamAccountName Sapphire -GroupCategory Security -GroupScope Global -DisplayName "Sapphire" -Path "CN=Users,DC=vdom,DC=local"
 
@@ -26,15 +26,25 @@ New-ADUser -Name "User Three" -GivenName "User" -Surname "Three" -SamAccountName
 
 Add-ADGroupMember -Identity $(Get-ADGroup -Identity "CN=Sapphire,CN=Users,DC=vdom,DC=local") -Members $(Get-ADUser -Identity "CN=User Three,CN=Users,DC=vdom,DC=local")
 
-# ------------------------------------------------------
+# --------------------------------------------------------
 #   Add Stones group and add Ruby, Sapphire as members
-# ------------------------------------------------------
+# --------------------------------------------------------
 
 New-ADGroup -Name "Stones" -SamAccountName Stones -GroupCategory Security -GroupScope Global -DisplayName "Stones" -Path "CN=Users,DC=vdom,DC=local"
 
 Add-ADGroupMember -Identity $(Get-ADGroup -Identity "CN=Stones,CN=Users,DC=vdom,DC=local") -Members $(Get-ADGroup -Identity "CN=Sapphire,CN=Users,DC=vdom,DC=local")
 
 Add-ADGroupMember -Identity $(Get-ADGroup -Identity "CN=Stones,CN=Users,DC=vdom,DC=local") -Members $(Get-ADGroup -Identity "CN=Ruby,CN=Users,DC=vdom,DC=local")
+
+# --------------------------------------------------------
+#   Add User Three and User One as members of group Odd
+# --------------------------------------------------------
+
+New-ADGroup -Name "Odd" -SamAccountName Odd -GroupCategory Security -GroupScope Global -DisplayName "Odd" -Path "CN=Users,DC=vdom,DC=local"
+
+Add-ADGroupMember -Identity $(Get-ADGroup -Identity "CN=Odd,CN=Users,DC=vdom,DC=local") -Members $(Get-ADUser -Identity "CN=User One,CN=Users,DC=vdom,DC=local")
+
+Add-ADGroupMember -Identity $(Get-ADGroup -Identity "CN=Odd,CN=Users,DC=vdom,DC=local") -Members $(Get-ADUser -Identity "CN=User Three,CN=Users,DC=vdom,DC=local")
 
 
 # To list all users
